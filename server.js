8a74da5e2e704f873743071122cd1c9d9d2a7cc4
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-01-22 20:46:13 -0500

    commets added

--- server.js
+++ server.js
@@ -13,15 +13,16 @@ const app = express();
 app.use(express.urlencoded({ extended: true }))
 //parse incoming json
 app.use(express.json())
+//middleware for serving static files
 app.use(express.static('public'))
 
 
-//function to start website with the html.index
+//route to start website with the html.index
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-//functun to open notes 
+//route to open notes 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
@@ -31,7 +32,7 @@ app.get('/notes', (req, res) => {
 app.get('/api/notes', (req, res) => {
     res.json(data)
 })
-
+//post route
 app.post('/api/notes', (req, res) => {
 
     let jsonFilePath = path.join(__dirname, "/db/db.json");
@@ -50,22 +51,25 @@ app.post('/api/notes', (req, res) => {
 
     newNote.id = highestId + 1;
     data.push(newNote)
-
+//function to write the note which is writin and checkin if we have err or not
     function createNote() {
         fs.writeFile(jsonFilePath, JSON.stringify(data), (err) => err ? console.log(err) : console.log("Your note is saved!"))
     }
     res.json(createNote)
 })
-
+//delete route
 app.delete('/api/notes/:id', (req, res) => {
 
     let jsonFilePath = path.join(__dirname, '/db/db.json')
+    //with loop we are finding note by 
     for (let i = 0; i < data.length; i++) {
         if (data[i].id == req.params.id) {
+            //splice the note out of the array
             data.splice(i, 1);
             break;
         }
     }
+    //write back the array without spliced note
     fs.writeFileSync(jsonFilePath, JSON.stringify(data), (err) => err ? console.log(err) : console.log('Your note is deleted!'))
 
     res.json(data)
